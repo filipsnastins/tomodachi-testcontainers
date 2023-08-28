174f42ae55f4263b8dc2ebbbd557903a1acdb689
Filips Nastins <nastinsfilips@gmail.com>
2023-08-28 09:28:56 +0200

    test: style

--- tests/containers/common/test_copy_folder_to_container.py
+++ tests/containers/common/test_copy_folder_to_container.py
@@ -3,8 +3,7 @@ from typing import Generator, cast
 
 import pytest
 
-from tomodachi_testcontainers.containers import copy_folder_to_container
-from tomodachi_testcontainers.containers import DockerContainer
+from tomodachi_testcontainers.containers import DockerContainer, copy_folder_to_container
 
 
 class AlpineContainer(DockerContainer):
@@ -29,9 +28,11 @@ def test_copy_folder_to_container(alpine_container: AlpineContainer) -> None:
     code, output = alpine_container.exec("find /tmp -type f")
     assert code == 0
     assert set(output.decode("utf-8").strip().split("\n")) == {"/tmp/nested/file-2.txt", "/tmp/file-1.txt"}
+
     code, output = alpine_container.exec("cat /tmp/file-1.txt")
     assert code == 0
     assert output.decode("utf-8") == "file 1\n"
+
     code, output = alpine_container.exec("cat /tmp/nested/file-2.txt")
     assert code == 0
     assert output.decode("utf-8") == "file 2\n"
