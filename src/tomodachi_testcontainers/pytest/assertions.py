950da12b45554a5ee00fb9d95d595ba25bf74dc9
Filips Nastins <nastinsfilips@gmail.com>
2023-10-02 10:33:51 +0200

    feat: add assert_logs_contain and assert_logs_not_contain

--- src/tomodachi_testcontainers/pytest/assertions.py
+++ src/tomodachi_testcontainers/pytest/assertions.py
@@ -1,4 +1,7 @@
 from datetime import datetime, timedelta, timezone
+from typing import Tuple
+
+from tomodachi_testcontainers.containers.common import DockerContainer
 
 UUID4_PATTERN = r"[0-9a-f]{8}-?[0-9a-f]{4}-?4[0-9a-f]{3}-?[89ab][0-9a-f]{3}-?[0-9a-f]{12}"
 
@@ -10,3 +13,18 @@ def assert_datetime_within_range(value: datetime, range: timedelta = DEFAULT_DAT
     start_datetime = current_datetime - range
     end_datetime = current_datetime + range
     assert start_datetime <= value <= end_datetime  # nosec: B101
+
+
+def assert_logs_contain(container: DockerContainer, expected: str) -> None:
+    streams: Tuple[str, str] = container.get_logs()
+    for stream in streams:
+        if expected in stream:
+            return
+    raise AssertionError(f"Expected logs to contain: '{expected}'")
+
+
+def assert_logs_not_contain(container: DockerContainer, expected: str) -> None:
+    streams: Tuple[str, str] = container.get_logs()
+    for stream in streams:
+        if expected in stream:
+            raise AssertionError(f"Expected logs not to contain: '{expected}'")
--- tests/pytest/test_assertions.py
+++ tests/pytest/test_assertions.py
@@ -1,10 +1,17 @@
 import datetime
 import re
 import uuid
+from unittest.mock import Mock
 
 import pytest
 
-from tomodachi_testcontainers.pytest.assertions import UUID4_PATTERN, assert_datetime_within_range
+from tomodachi_testcontainers import DockerContainer
+from tomodachi_testcontainers.pytest.assertions import (
+    UUID4_PATTERN,
+    assert_datetime_within_range,
+    assert_logs_contain,
+    assert_logs_not_contain,
+)
 
 
 def test_match_uuid4_pattern() -> None:
@@ -23,3 +30,26 @@ def test_assert_datetime_within_range() -> None:
         assert_datetime_within_range(now - datetime.timedelta(seconds=10))
     with pytest.raises(AssertionError):
         assert_datetime_within_range(now + datetime.timedelta(seconds=11))
+
+
+def test_logs_contain() -> None:
+    mock_container = Mock(spec_set=DockerContainer)
+    mock_container.get_logs = Mock(return_value=("stdout\n", "stderr\n"))
+
+    assert_logs_contain(mock_container, "stdout")
+    assert_logs_contain(mock_container, "stderr")
+
+    with pytest.raises(AssertionError, match=r"Expected logs to contain: 'foo'"):
+        assert_logs_contain(mock_container, "foo")
+
+
+def test_logs_not_contain() -> None:
+    mock_container = Mock(spec_set=DockerContainer)
+    mock_container.get_logs = Mock(return_value=("stdout\n", "stderr\n"))
+
+    assert_logs_not_contain(mock_container, "foo")
+
+    with pytest.raises(AssertionError, match=r"Expected logs not to contain: 'stdout'"):
+        assert_logs_not_contain(mock_container, "stdout")
+    with pytest.raises(AssertionError, match=r"Expected logs not to contain: 'stderr'"):
+        assert_logs_not_contain(mock_container, "stderr")
