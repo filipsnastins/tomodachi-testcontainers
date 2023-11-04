f093820e28fb0a891ae23eb5995a53c16d20d2c4
Filips Nastins <nastinsfilips@gmail.com>
2023-11-04 10:16:00 +0100

    refactor(clients.snssqs): rename module snssqs_client -> snssqs

--- src/tomodachi_testcontainers/clients/__init__.py
+++ src/tomodachi_testcontainers/clients/__init__.py
@@ -1,4 +1,4 @@
-from tomodachi_testcontainers.clients.snssqs_client import SNSSQSTestClient
+from tomodachi_testcontainers.clients.snssqs import SNSSQSTestClient
 
 __all__ = [
     "SNSSQSTestClient",
