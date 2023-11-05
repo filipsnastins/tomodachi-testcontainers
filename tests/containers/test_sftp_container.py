e36601bd3ada0e365ce18a4f189412d2e401d380
Filips Nastins <nastinsfilips@gmail.com>
2023-11-05 15:04:44 +0100

    test(containers.sftp): use sftp client fixture in all tests

--- tests/containers/test_sftp_container.py
+++ tests/containers/test_sftp_container.py
@@ -1,60 +1,34 @@
 import tempfile
+import uuid
 
 import asyncssh
 import pytest
 
-from tomodachi_testcontainers import SFTPContainer
+
+@pytest.mark.asyncio()
+async def test_sftp_container__authenticate_with_password(userpass_sftp_client: asyncssh.SFTPClient) -> None:
+    filename = f"{uuid.uuid4()}.txt"
+
+    with tempfile.NamedTemporaryFile() as f:
+        f.write(b"Hello, World!")
+        f.seek(0)
+        await userpass_sftp_client.put(f.name, f"upload/{filename}")
+
+    listdir = await userpass_sftp_client.listdir("upload")
+    assert set(listdir) == {".", "..", filename}
 
 
 @pytest.mark.asyncio()
-async def test_sftp_container__authenticate_with_password(sftp_container: SFTPContainer) -> None:
-    async with asyncssh.connect(
-        host="localhost",
-        port=sftp_container.edge_port,
-        username="userpass",
-        password="pass",
-        known_hosts=sftp_container.get_known_hosts(),
-    ) as conn:
-        async with conn.start_sftp_client() as sftp:
-            cwd = await sftp.getcwd()
-            assert cwd == "/"
+async def test_sftp_container__authenticate_with_ssh_keys(userssh_sftp_client: asyncssh.SFTPClient) -> None:
+    filename = f"{uuid.uuid4()}.txt"
 
-            await sftp.chdir("upload")
-            cwd = await sftp.getcwd()
-            assert cwd == "/upload"
+    cwd = await userssh_sftp_client.getcwd()
+    assert cwd == "/"
 
-            with tempfile.NamedTemporaryFile() as f:
-                f.write(b"Hello, World!")
-                f.seek(0)
+    with tempfile.NamedTemporaryFile() as f:
+        f.write(b"Hello, World!")
+        f.seek(0)
+        await userssh_sftp_client.put(f.name, f"upload/{filename}")
 
-                await sftp.put(f.name, "hello-world.txt")
-
-            listdir = await sftp.listdir(".")
-            assert set(listdir) == {".", "..", "hello-world.txt"}
-
-
-@pytest.mark.asyncio()
-async def test_sftp_container__authenticate_with_ssh_keys(sftp_container: SFTPContainer) -> None:
-    async with asyncssh.connect(
-        host="localhost",
-        port=sftp_container.edge_port,
-        username="userssh",
-        client_keys=sftp_container.authorized_private_key,
-        known_hosts=sftp_container.get_known_hosts(),
-    ) as conn:
-        async with conn.start_sftp_client() as sftp:
-            cwd = await sftp.getcwd()
-            assert cwd == "/"
-
-            await sftp.chdir("upload")
-            cwd = await sftp.getcwd()
-            assert cwd == "/upload"
-
-            with tempfile.NamedTemporaryFile() as f:
-                f.write(b"Hello, World!")
-                f.seek(0)
-
-                await sftp.put(f.name, "hello-world.txt")
-
-            listdir = await sftp.listdir(".")
-            assert set(listdir) == {".", "..", "hello-world.txt"}
+    listdir = await userssh_sftp_client.listdir("upload")
+    assert set(listdir) == {".", "..", filename}
