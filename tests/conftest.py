4c9d2cbb73e268f6b75e7e3c6ede230e76e0548e
Filips Nastins <nastinsfilips@gmail.com>
2023-11-05 15:05:12 +0100

    test: refactor test containers in fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,7 @@ from typing import Generator, Iterator, cast
 
 import pytest
 
-from tomodachi_testcontainers import DockerContainer
+from tomodachi_testcontainers import DockerContainer, WebContainer
 from tomodachi_testcontainers.utils import get_available_port
 
 pytest_plugins = ["pytester"]
@@ -13,23 +13,18 @@ pytest_plugins = ["pytester"]
 class AlpineContainer(DockerContainer):
     def __init__(self) -> None:
         super().__init__(image="alpine:latest")
+        self.with_command("sleep infinity")
 
     def log_message_on_container_start(self) -> str:
         return "Alpine container started"
 
 
-class HTTPBinContainer(DockerContainer):
+class HTTPBinContainer(WebContainer):
     def __init__(self) -> None:
-        super().__init__(image="kennethreitz/httpbin")
-        self.edge_port = get_available_port()
-        self.with_bind_ports(80, self.edge_port)
+        super().__init__(image="kennethreitz/httpbin", internal_port=80, edge_port=get_available_port())
 
     def log_message_on_container_start(self) -> str:
         return "HTTPBin container started"
-
-    def get_external_url(self) -> str:
-        host = self.get_container_host_ip()
-        return f"http://{host}:{self.edge_port}"
 
 
 @pytest.fixture(scope="session")
@@ -38,13 +33,13 @@ def event_loop() -> Iterator[asyncio.AbstractEventLoop]:
         yield loop
 
 
-@pytest.fixture()
+@pytest.fixture(scope="session")
 def alpine_container() -> Generator[AlpineContainer, None, None]:
-    with AlpineContainer().with_command("sleep 10") as container:
+    with AlpineContainer() as container:
         yield cast(AlpineContainer, container)
 
 
-@pytest.fixture()
+@pytest.fixture(scope="session")
 def httpbin_container() -> Generator[HTTPBinContainer, None, None]:
     with HTTPBinContainer() as container:
         yield cast(HTTPBinContainer, container)
