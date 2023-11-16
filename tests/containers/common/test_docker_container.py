73391f62a8e5969a1d3ba00ff9a6ce80c95967f0
Filips Nastins <nastinsfilips@gmail.com>
2023-11-16 19:02:17 +0100

    test: refactor docker container test

--- tests/containers/common/test_docker_container.py
+++ tests/containers/common/test_docker_container.py
@@ -10,37 +10,54 @@ from tomodachi_testcontainers.pytest.async_probes import probe_until
 class WorkingContainer(DockerContainer):
     def __init__(self) -> None:
         super().__init__(image="alpine:latest")
+        self.with_command("sleep infinity")
 
     def log_message_on_container_start(self) -> str:
         return "Working container started"
 
 
-class FailingContainer(DockerContainer):
+class FailingHealthcheckContainer(DockerContainer):
     def __init__(self) -> None:
         super().__init__(image="alpine:latest")
+        self.with_command("sleep infinity")
 
     def log_message_on_container_start(self) -> str:
         return "Container with a broken healthcheck started"
 
-    def start(self) -> "FailingContainer":
+    def start(self) -> "FailingHealthcheckContainer":
         super().start()
-        self.exec("sh -c 'echo \"container startup failed\" >> /proc/1/fd/1'")
-        raise RuntimeError("Failed to start the container")
+        self.exec("sh -c 'echo \"container healthcheck failed\" >> /proc/1/fd/1'")
+        raise RuntimeError("Container healthcheck failed")
 
 
 class TestCleanup:
     def test_container_started(self) -> None:
         container_name = shortuuid.uuid()
 
-        _ = WorkingContainer().with_name(container_name).with_command("sleep infinity").start()
+        _ = WorkingContainer().with_name(container_name).start()
 
         assert docker.from_env().containers.get(container_name)
+
+    def test_container_removed_on_stop(self) -> None:
+        container_name = shortuuid.uuid()
+        container = WorkingContainer().with_name(container_name).start()
+
+        container.stop()
+
+        with pytest.raises(docker.errors.NotFound):
+            docker.from_env().containers.get(container_name)
+
+    def test_stop_container_is_idempotent(self) -> None:
+        container = WorkingContainer().start()
+
+        container.stop()
+        container.stop()
 
     @pytest.mark.asyncio()
     async def test_container_removed_on_garbage_collection(self) -> None:
         container_name = shortuuid.uuid()
 
-        WorkingContainer().with_name(container_name).with_command("sleep infinity").start()
+        WorkingContainer().with_name(container_name).start()
 
         # Since we don't have a reference (variable) to the container object,
         # it is eventually garbage collected and removed
@@ -52,17 +69,8 @@ class TestCleanup:
 
     def test_container_removed_on_context_manager_exit(self) -> None:
         container_name = shortuuid.uuid()
-        with WorkingContainer().with_name(container_name).with_command("sleep infinity"):
+        with WorkingContainer().with_name(container_name):
             pass
-
-        with pytest.raises(docker.errors.NotFound):
-            docker.from_env().containers.get(container_name)
-
-    def test_container_removed_on_stop(self) -> None:
-        container_name = shortuuid.uuid()
-        container = WorkingContainer().with_name(container_name).with_command("sleep infinity").start()
-
-        container.stop()
 
         with pytest.raises(docker.errors.NotFound):
             docker.from_env().containers.get(container_name)
@@ -82,23 +90,23 @@ class TestCleanup:
 
 class TestLogging:
     def test_container_logs_are_forwarded_on_context_manager_exit(self, capsys: pytest.CaptureFixture) -> None:
-        with WorkingContainer().with_command("sleep infinity") as container:
+        with WorkingContainer() as container:
             container.exec("sh -c 'echo \"my log message\" >> /proc/1/fd/1'")
 
         stderr = str(capsys.readouterr().err)
         assert "--- Logging error ---" not in stderr  # The error message comes from logger standard library
         assert "my log message" in stderr
 
-    def test_container_logs_are_forwarded_on_failed_startup(self, capsys: pytest.CaptureFixture) -> None:
-        with pytest.raises(RuntimeError), FailingContainer().with_command("sleep infinity"):
+    def test_container_logs_are_forwarded_on_failed_healthcheck(self, capsys: pytest.CaptureFixture) -> None:
+        with pytest.raises(RuntimeError), FailingHealthcheckContainer():
             pass
 
         stderr = str(capsys.readouterr().err)
         assert "--- Logging error ---" not in stderr
-        assert "container startup failed" in stderr
+        assert "container healthcheck failed" in stderr
 
     def test_container_logs_are_not_forwarded_outside_of_context_manager(self, capsys: pytest.CaptureFixture) -> None:
-        container = WorkingContainer().with_command("sleep infinity").start()
+        container = WorkingContainer().start()
 
         container.exec("sh -c 'echo \"my log message\" >> /proc/1/fd/1'")
 
@@ -107,7 +115,7 @@ class TestLogging:
         assert "my log message" not in stderr
 
     def test_logs_prefixed_with_container_name(self, capsys: pytest.CaptureFixture) -> None:
-        with WorkingContainer().with_command("sleep infinity").with_name("my-container-name"):
+        with WorkingContainer().with_name("my-container-name"):
             pass
 
         stderr = str(capsys.readouterr().err)
