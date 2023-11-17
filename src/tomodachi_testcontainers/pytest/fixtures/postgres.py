cfe00398d925cade028afa650731aedf9a52449b
Filips Nastins <nastinsfilips@gmail.com>
2023-11-17 08:44:57 +0100

    chore(PostgreSQLContainer): use default version tag

--- src/tomodachi_testcontainers/pytest/fixtures/postgres.py
+++ src/tomodachi_testcontainers/pytest/fixtures/postgres.py
@@ -9,6 +9,6 @@ from tomodachi_testcontainers.utils import get_available_port
 
 @pytest.fixture(scope="session")
 def postgres_container() -> Generator[PostgreSQLContainer, None, None]:
-    image = os.getenv("POSTGRES_TESTCONTAINER_IMAGE_ID", "postgres:16-bullseye")
+    image = os.getenv("POSTGRES_TESTCONTAINER_IMAGE_ID", "postgres:16")
     with PostgreSQLContainer(image=image, edge_port=get_available_port()) as container:
         yield cast(PostgreSQLContainer, container)
--- tests/pytest/test_postgres_fixtures.py
+++ tests/pytest/test_postgres_fixtures.py
@@ -10,7 +10,7 @@ def test_postgres_image_name_set_from_envvar(pytester: pytest.Pytester) -> None:
             import os
 
 
-            os.environ["POSTGRES_TESTCONTAINER_IMAGE_ID"] = "postgres:15.4-alpine3.18"
+            os.environ["POSTGRES_TESTCONTAINER_IMAGE_ID"] = "postgres:16-alpine"
             """
         )
     )
@@ -22,7 +22,7 @@ def test_postgres_image_name_set_from_envvar(pytester: pytest.Pytester) -> None:
 
 
             def test_postgres_image_name_set_from_envvar(postgres_container: PostgreSQLContainer) -> None:
-                assert postgres_container.image == "postgres:15.4-alpine3.18"
+                assert postgres_container.image == "postgres:16-alpine"
             """
         )
     )
