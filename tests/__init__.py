83f3740e3dc3a44e8124bfb66fc03bf028198686
Filips Nastins <nastinsfilips@gmail.com>
2023-12-18 09:24:35 +0100

    fix(DynamoDBAdminContainer): set correct aws_region envvar

--- tests/__init__.py
+++ tests/__init__.py
@@ -5,6 +5,7 @@ https://docs.getmoto.org/en/latest/docs/getting_started.html?highlight=muta#how-
 """
 import os
 
+os.environ["AWS_REGION"] = "us-east-1"
 os.environ["AWS_DEFAULT_REGION"] = "us-east-1"
 os.environ["AWS_ACCESS_KEY_ID"] = "testing"
 os.environ["AWS_SECRET_ACCESS_KEY"] = "testing"
