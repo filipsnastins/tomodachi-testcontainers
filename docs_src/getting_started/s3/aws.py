b97306a6f1231d8b6dbda354a7a83b90e05b62b6
Filips Nastins <nastinsfilips@gmail.com>
2024-01-09 18:46:44 +0100

    doc: wip

--- docs_src/getting_started/s3/aws.py
+++ docs_src/getting_started/s3/aws.py
@@ -1,5 +1,6 @@
 import os
 from contextlib import AsyncExitStack
+from typing import Tuple
 
 from aiobotocore.session import get_session
 from types_aiobotocore_s3 import S3Client
@@ -11,7 +12,7 @@ def get_bucket_name() -> str:
     return os.environ["AWS_S3_BUCKET_NAME"]
 
 
-async def create_s3_client() -> tuple[S3Client, AsyncExitStack]:
+async def create_s3_client() -> Tuple[S3Client, AsyncExitStack]:
     exit_stack = AsyncExitStack()
     client = await exit_stack.enter_async_context(
         session.create_client(
